fe1a01c3a36983e7147ff64ff39a4ddad8c7db58
pborisp <popovbs@gmail.com>
2025-02-21 01:44:56 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Задача №1
+        System.out.println("Задача №1:");
+        int[] cost = new int[5];
+        for (int i = 0; i < cost.length; i++) {
+            cost[i] = (int) (Math.random() * 100);
+            System.out.print(cost[i] + " ");
+        }
+        System.out.println();
+        int summCost = 0;
+        for (int i = 0; i < cost.length; i++) {
+            summCost += cost[i];
+        }
+        System.out.println("Сумма трат за месяц составила: " + summCost + " рублей");
     }
 }
